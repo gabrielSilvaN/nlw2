7566d0c9ba2d969d74e967f9eab4a0d6b67f943f
gabrielSilvaN <gabriel_silva@discente.ufg.br>
2020-08-05 00:54:33 -0300

    finalização api

--- backend/src/controllers/ClassesController.ts
+++ backend/src/controllers/ClassesController.ts
@@ -13,7 +13,38 @@ interface ScheduleItem {
 export default class ClassesController {
 
   async index(request: Request, response: Response) {
-    
+    const filters = request.query;
+
+    const week_day = filters.week_day as string;
+    const subject = filters.subject as string;
+    const time = filters.time as string;
+
+
+
+    if(!week_day || !subject || !time) {
+      return response.status(400).json({error: "Missing filters to searche classes"});
+    }
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+    .whereExists(function(){
+      this.select('class_schedule.*')
+      .from('class_schedule')
+      .whereRaw('`class_schedule`.`class_id` = `classes`.`id`') //whereRaw nos permite criar um where 'próprio' (customizado)
+      .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+      .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+      .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+
+
+    })
+    .where('classes.subject', '=', subject)
+    .join('users', 'classes.user_id', '=', 'users.id')
+    .select(['classes.*', 'users.*']);
+
+    return response.json(classes);
+
+
   }
 
   async create(request: Request, response: Response) {
